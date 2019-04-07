3ead1b811e8ab8830b60e4eaf0dc68f6d275cabd
baconPancakes123 <CHeffernan087@gmail.com>
2019-04-07 02:43:24 +0100

    basic encryption done. How are we gonna do the key management stuff who knows. its 3am im going to bed. Nothings working

--- keyManagmentSystem/kms/encrypt.py
+++ keyManagmentSystem/kms/encrypt.py
@@ -1,32 +1,48 @@
-import Crypto
-from Crypto.PublicKey import RSA
-from Crypto import Random
-import ast
+import cryptography
 
-random_generator = Random.new().read
-key = RSA.generate(1024, random_generator) #generate pub and priv key
-
-publickey = key.publickey() # pub key export for exchange
-
-encrypted = publickey.encrypt('encrypt this message', 32)
-#message to encrypt is in the above line 'encrypt this message'
-
-print('encrypted message:'), encrypted #ciphertext
-f = open ('encryption.txt', 'w')
-f.write(str(encrypted)) #write ciphertext to file
-f.close()
-
-#decrypted code below
-
-f = open('encryptedFiles/encryption.txt', 'r')
-message = f.read()
+from cryptography.fernet import Fernet
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import sys
 
 
-decrypted = key.decrypt(ast.literal_eval(str(encrypted)))
+fileName = input("What is the name of the file you would like to encrypt?")
 
-print('decrypted' +decrypted)
 
-f = open ('/encryption.txt', 'w')
-f.write(str(message))
-f.write(str(decrypted))
-f.close()
+key = Fernet.generate_key()
+
+file = open('key.key', 'wb')
+file.write(key) # The key is type bytes still
+file.close()
+
+file = open('key.key', 'rb')
+key = file.read() # The key will be type bytes
+file.close()
+
+
+password_provided = "password" # This is input in the form of a string
+password = password_provided.encode() # Convert to type bytes
+salt = b'salt_' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+
+message = "my deep dark secret".encode()
+
+f = Fernet(key)
+encrypted = f.encrypt(message)
+print(encrypted)
+
+print("\n\n")
+
+decrypted = f.decrypt(encrypted)
+print(decrypted)
